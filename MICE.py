e9cf61a6a6c40cbc9b4076da68156603ce615f14
jomanovic <47547826+jomanovic@users.noreply.github.com>
2019-06-07 23:56:33 -0400

    Update MICE.py
    Removed correlation threshold as it doesn't prevent multicoliniarity

--- MICE.py
+++ MICE.py
@@ -19,7 +19,7 @@ class MiceImputer(object):
         self.copy = copy
         self.imp = SimpleImputer(strategy=self.strategy, copy=self.copy)
 
-    def fit_transform(self, X, method = 'Linear', corr_thrs = 0, iter = 5, verbose = True):
+    def fit_transform(self, X, method = 'Linear', iter = 5, verbose = True):
         
         # Why use Pandas?
         # http://gouthamanbalaraman.com/blog/numpy-vs-pandas-comparison.html
@@ -72,20 +72,6 @@ class MiceImputer(object):
                     m.fit(train_x, train_y)
                     new_X.loc[null_rows,column] = pd.Series(m.predict(test_x))
                     not_null_cols.append(column)
-                
-       
-        ### Create a dictionary { cols : [ other_cols if corr(cols, other_cols) < threshold ] }  (see "multicollinearity")
-        
-        corr_dict = {}
-        
-        if corr_thrs:
-            if verbose:
-                print('Building correlation table for missing-values...')
-            for col in null_cols:
-                corr_dict[col] = []
-                for tmp in new_X.columns:
-                    if corr_thrs < abs(new_X[col].corr(new_X[tmp])) and col != tmp:   
-                        corr_dict[col].append(tmp)
         
         ### Begin iterations of MICE
         
@@ -103,15 +89,8 @@ class MiceImputer(object):
                 not_null_y = new_X.loc[~null_rows, column]
                 not_null_X = new_X[~null_rows].drop(column, axis = 1)
                 
-                train_x, val_x, train_y, val_y = train_test_split(not_null_X, not_null_y, test_size=0.33, random_state=42)
-                
+                train_x, val_x, train_y, val_y = train_test_split(not_null_X, not_null_y, test_size=0.33, random_state=42)  
                 test_x = new_X.drop(column, axis = 1)
-                
-                if corr_thrs and corr_dict.get(column, []):  
-                    corr_cols = corr_dict[column]
-                    train_x = train_x[corr_cols]
-                    test_x = test_x[corr_cols]
-                    val_x = val_x[corr_cols]
                   
                 if new_X[column].nunique() > 2:
                     if method == 'Linear':
